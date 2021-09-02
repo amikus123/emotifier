18742fcddfc6d120b0df8d10de5b4e63f149ea8a
Amadeusz <amikusek123@gmail.com>
2021-09-02 23:11:10 +0200

    added option of pasting emojis

--- client/app/utils/forms/emojiInputs.ts
+++ client/app/utils/forms/emojiInputs.ts
@@ -1,17 +1,20 @@
-export const allowDeletesGenerator = (setText:(arg0: any)=>any) => {
- const res = (e:any)=>{
-  try{
-    // allows ony delete and backspace
-  if (
-    e.nativeEvent.inputType === "deleteContentBackward" ||
-    e.nativeEvent.inputType === "deleteContentBackward"
-  ) {
-    setText(e.target.value);
-  }
-  }catch(err){
-    console.error(err)
-  }
- }
- return res
-  
-}
+const emojiRegexRGI = require("emoji-regex/RGI_Emoji.js");
+
+export const allowDeletesGenerator = (setText: (arg0: any) => any) => {
+  // Generates function which allows user to manually
+  // enter emojis into emoji inputs
+
+  const res = (e: any) => {
+    const regex = emojiRegexRGI();
+    let match;
+    let emojiString = "";
+
+    while ((match = regex.exec(e.target.value))) {
+      const emoji = match[0];
+
+      emojiString += emoji;
+    }
+    setText(emojiString);
+  };
+  return res;
+};
